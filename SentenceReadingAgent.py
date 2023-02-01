ba023548fe7807c7867903f4a28baee2cb4ed1d1
tutorcs <tutorcs@163.com>
2023-02-01 12:35:03 +0800

    WeChat cstutorcs CS7637 KBAI Mini Project 3 SentenceReadingAgent

---
+++ SentenceReadingAgent.py
@@ -0,0 +1,14 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+class SentenceReadingAgent:
+    def __init__(self):
+        #If you want to do any initial processing, add it here.
+        pass
+
+    def solve(self, sentence, question):
+        #Add your code here! Your solve method should receive
+		#two strings as input: sentence and question. It should
+		#return a string representing the answer to the question.
+        pass
